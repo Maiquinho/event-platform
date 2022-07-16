4fa410b3ae8b8252466bfeddd908ab495e620e97
Michael Matheus <michaelmatheus2001@gmail.com>
2022-07-16 14:27:38 -0300

    feat: video plugin style + react router

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,18 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
+
+import '@vime/core/themes/default.css'
 
 export function Video() {
     return (
         <div className="flex-1">
             <div className="bg-black">
-                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+                    <Player>
+                        <Youtube videoId="sNL_zGATBYM" />
+                        <DefaultUi />
+                    </Player>
+                </div>
             </div>
 
             <div className="p-8 max-w-[1100px] mx-auto">
